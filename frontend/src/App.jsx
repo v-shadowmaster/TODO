a3034df8bc4e71ef85ba46f3670fde192f8ad4f8
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-02-02 16:02:27 +0530

    asdfasdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
-
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<Signup />} />
-    </Routes>
-  </Router>
-);
-
+import Login from "./components/Login";
 function App() {
-  return <div>{routes}</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
