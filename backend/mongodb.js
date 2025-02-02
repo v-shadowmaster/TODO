64433fbcc6c34271b797e78d394f158964781e97
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-02-02 13:50:44 +0530

    started with the new project

--- backend/mongodb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () =>
-  mongoose
-    .connect("mongodb://127.0.0.1:27017/todo_app")
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log("MongoDB connected failed ", err));
-
-module.exports = connectDB;
